9c2b03aee8a30110a97d13f7859f004e092a08d4
cozz-py <bencozzens13@gmail.com>
2023-02-22 07:53:43 -0500

    Add project files.

---
+++ RandomMenu/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    class MenuItem
+    {
+        public Random Randomizer = new Random();
+
+        public string[] Proteins = { "roast beef", "salami", "turkey", "ham", "pastrami", "tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "brioche" };
+
+        public string Description = "";
+        public string Price;
+    }
+}
